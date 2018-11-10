2fc08aaef257ad19a5397ae4e8ae41e76cc3e183
ryankennedy712 <ryankennedy712@gmail.com>
2018-11-10 13:15:13 -0500

    new

---
+++ Battleship - Ryan.py
@@ -0,0 +1,49 @@
+from random import randint
+
+l = [] #empty list to append later
+def BoatMarker(Xcord,Ycord):
+    l[Ycord][Xcord] = "U"
+
+def hitmarker(Xcord,Ycord):
+    l[Ycord][Xcord] = "x"
+
+def userinput(opt=""):
+    if opt == "ship":
+        PlayerGuess = input("Where should we place the ship? (x,y): ")
+        x = str(find(PlayerGuess[x], beg=0,end ="." )) - 1
+        y = str(find(PlayerGuess[y], beg=".", end = "")) -1
+        BoatMarker(x,y)
+    if opt == "strike":
+        PlayerGuess = input("Missle strike to space? (x,y): ")
+        x = str(find(PlayerGuess[x], beg=0,end ="." )) - 1
+        y = str(find(PlayerGuess[y], beg=".", end = "")) -1
+        hitmarker(x,y)
+
+def BoardGUI(opt="Generate"):#game board
+    if opt == "Generate":
+        for x in range(10):
+            l.append(["_"] * 10)
+    elif opt == "Update":
+        board = ""
+        for b in range(10):
+            for j in range(10):
+                board = board + l[b][j]
+                board = board + " "
+            board = board + "\n"
+        print(board)
+        return board
+
+
+
+
+
+
+BoardGUI() #generate
+for i in range(5):
+    userinput("ship")
+    BoardGUI("Update")
+print("DONE PLACING SHIPS!")
+for i in range(5):
+    userinput("strike")
+    BoardGUI("Update")
+print("DONE STRIKING!")
